9e74f81a2b2464157f7c82f24bfe9587c7160354
Dea Krasnić <52324941+suncica-negra@users.noreply.github.com>
2024-04-12 19:15:15 +0200

    Update main.js

--- static/main.js
+++ static/main.js
@@ -1,6 +1,8 @@
-const dropdown = document.querySelector('.dropdown');
-const loaderWrapper = document.querySelector('.loader_wrapper');
 const isMob = window.innerWidth < 900;
+let dropdown = null;
+let loaderWrapper = null;
+let targetUnit = null;
+let selectedUnit = null;
 let electionResults = {};
 
 function getRandomColor() {
@@ -15,24 +17,30 @@ function getRandomColor() {
 }
 
 function setLiveOrNot(live) {
-    if (!live) document.querySelector('.live').style.visibility = 'hidden';
+    const liveElement = document.querySelector('.live');
+    if (liveElement && !live) liveElement.style.visibility = 'hidden';
 }
 
 function fixTooltipPosition() {
     const isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+    if (isSafari) document.documentElement.style.setProperty('--textShadow', '0 0 1.3px #090909');
+
     const allTooltips = document.querySelectorAll('.tooltip_wrapper .tooltip');
 
-    allTooltips.forEach(e => {
+    allTooltips?.forEach(e => {
         if (isSafari) e.classList.add('after_safari');
 
         if (!isMob) {
             const tooltipPosition = e.getBoundingClientRect();
 
-            if ((tooltipPosition.x + tooltipPosition.width) > window.innerWidth) {
-                e.parentNode.style.left = 'unset';
-                e.parentNode.style.right = '90%';
+            if (tooltipPosition && ((tooltipPosition.x + tooltipPosition.width) > window.innerWidth)) {
+                const parent = e.parentNode;
+                if (parent) {
+                    parent.style.left = 'unset';
+                    parent.style.right = '90%';
+                    parent.style.setProperty('--tooltipPadding', '10px 20px 10px 5px');
+                }
                 e.classList.add('hide_after','display_before');
-                e.parentNode.style.setProperty('--tooltipPadding', '10px 20px 10px 5px');
 
                 if (isSafari) e.classList.add('hide_after','display_before_safari');
             }
@@ -40,29 +48,23 @@ function fixTooltipPosition() {
     });
 }
 
-function placeDataInHtml(responseData, rh = true, manjine = false) {
+function placeDataInHtml(responseData, rh = true) {
     document.getElementById('election_results').replaceChildren();
+
     const tooltipHeader = 'Lista stranaka unutar koalicije:';
-    const dataToProcess = rh ? responseData.ukupnoMandati : responseData;
+    const dataToProcess = rh ? responseData.ukupnoMandatiGrupirano : responseData;
     let maxMandates = 151;
 
     const fragment = new DocumentFragment();
 
-    dataToProcess.forEach((e, i) => {
+    dataToProcess?.forEach((e, i) => {
         if (i === 0) maxMandates = e.brMandata;
 
         const wrapperDiv = document.createElement('div');
         wrapperDiv.setAttribute('class', 'party_result_wrapper');
 
         const party = document.createElement('p');
-        let partyName = manjine ? '' : e.naziv;
-        if (manjine) {
-            e.lista.forEach((p, i) => {
-                if ((i + 1) === e.lista.length) partyName += p.naziv;
-                else partyName += `${p.naziv}, `;
-            });
-        }
-        party.textContent = partyName;
+        party.textContent = e.naziv;
         party.setAttribute('class', 'party');
         wrapperDiv.appendChild(party);
 
@@ -75,8 +77,15 @@ function placeDataInHtml(responseData, rh = true, manjine = false) {
                 graphicalRepresentation.style.height = `${(e.brMandata/maxMandates)*100}%`;
             }
         }, 150);
-        // ToDo: boja stranke!!
-        graphicalRepresentation.style.backgroundColor = e.color ? e.color : getRandomColor();
+
+        const graphColor = e.color ? e.color : getRandomColor();
+        graphicalRepresentation.style.backgroundColor = graphColor;
+
+        if (e.hover) {
+            graphicalRepresentation.style.setProperty('--graphHover', e.hover);
+            graphicalRepresentation.style.filter = 'unset';
+        } else graphicalRepresentation.style.setProperty('--graphHover', graphColor);
+
         graphicalRepresentation.setAttribute('class', 'graph');
 
         const graphWrapper = document.createElement('div');
@@ -99,13 +108,18 @@ function placeDataInHtml(responseData, rh = true, manjine = false) {
         tooltipP.textContent = tooltipHeader;
         divTag.appendChild(tooltipP);
         divTooltipWrapper.appendChild(divTag);
+
         const tooltipOlElement = document.createElement('ol');
-        const dataFoProcess = manjine ? e.lista[0].stranke : e.stranke;
-        dataFoProcess.forEach(s => {
+        let dataForProcess = e.political_parties;
+
+        if (!rh) dataForProcess = e.naziv.split(', ');
+
+        dataForProcess?.forEach(s => {
             const liTag = document.createElement('li');
-            liTag.textContent = s.naziv;
+            liTag.textContent = s.naziv || s;
             tooltipOlElement.appendChild(liTag);
         });
+
         divTag.appendChild(tooltipOlElement);
         wrapperDiv.appendChild(divTooltipWrapper);
         // tooltip end
@@ -116,70 +130,84 @@ function placeDataInHtml(responseData, rh = true, manjine = false) {
     const resultsElement = document.getElementById('election_results');
     resultsElement.appendChild(fragment);
 
-    loaderWrapper.style.display = 'none';
-
     fixTooltipPosition();
+
+    if (loaderWrapper) loaderWrapper.style.display = 'none';
 }
 
 function setUnitData(unit) {
-    if (unit === '012') placeDataInHtml(electionResults.izborne_liste_manjine, false, true);
-
-    else electionResults.izborne_jedinice.forEach(res => {
+    electionResults.izborne_jedinice?.forEach(res => {
         if (res.ijSifra.includes(unit)) placeDataInHtml(res.lista, false);
     });
 }
 
+function setProcessedVotesAndLastChange(status, refresh = true) {
+    const destination = document.getElementById('processed_votes_and_last_change');
+
+    if (!destination) return;
+
+    if (refresh) destination.replaceChildren();
+
+    const tempInfo = document.getElementById('processed_votes_and_last_change_template');
+    const cloneInfo = tempInfo.content.cloneNode(true);
+    const processedVotes = cloneInfo.getElementById('processed_votes');
+
+    processedVotes.textContent = (`${status?.bmPosto}%`) + processedVotes.textContent;
+
+    const lastChange = cloneInfo.getElementById('last_change');
+    const lastUpdated = (status?.vrijeme)?.split(":");
+    const paddedTime = lastUpdated?.map(i => i.padStart(2, '0'));
+    lastChange.textContent += `${paddedTime?.join(':')} H`;
+
+    destination.appendChild(cloneInfo);
+}
+
 function processData(responseData, click = false) {
-    if (!click) {
-        const tempInfo = document.getElementById('processed_votes_and_last_change_template');
-        const cloneInfo = tempInfo.content.cloneNode(true);
-        const processedVotes = cloneInfo.getElementById('processed_votes');
-        // ToDo: postotak obrađenih glasova!!
-        processedVotes.textContent = (responseData.processed_votes || '0%') + processedVotes.textContent;
-
-        const lastChange = cloneInfo.getElementById('last_change');
-        const date = new Date(responseData.time_updated);
-        const userTimezoneOffset = date.getTimezoneOffset() * 60000;
-        const newTime = new Date(date.getTime() - userTimezoneOffset);
-        const timeStrings = (newTime.toLocaleTimeString()).split(':');
-        const paddedTime = timeStrings.map(i => i.padStart(2, '0'));
-        // ToDo: provjeriti da li prikazuje dobro vrijeme!!
-        lastChange.textContent += `${paddedTime.join(':').substring(0,5)} H`;
-
-        const destination = document.getElementById('processed_votes_and_last_change');
-        destination.appendChild(cloneInfo);
-    }
+    if (!click) setProcessedVotesAndLastChange(responseData.status, false);
 
     placeDataInHtml(responseData);
+}
+
+function decideHowToProcess(unit) {
+    if (unit === '0') processData(electionResults, true);
+    else setUnitData(unit);
 }
  
 window.addEventListener('pointerup', (e) => {
     const svg = document.querySelector('svg');
 
-    if (e.composedPath().includes(dropdown)) {
+    if (dropdown && e.composedPath().includes(dropdown)) {
         if (dropdown.classList.contains('closed')) {
             dropdown.classList.remove('closed');
-            svg.classList.add('open');
+            svg?.classList.add('open');
         } else {
             dropdown.classList.add('closed');
-            svg.classList.remove('open');
+            svg?.classList.remove('open');
             const unit = e.target.dataset.unit;
 
             if (!unit) return;
 
-            loaderWrapper.style.display = 'block';
-            document.querySelector('#target_unit').textContent = e.target.textContent;
+            if (loaderWrapper) loaderWrapper.style.display = 'block';
 
-            if (unit === '0') processData(electionResults, true);
-            else setUnitData(unit);
+            if (targetUnit) targetUnit.textContent = e.target?.textContent;
+
+            decideHowToProcess(unit);
         }
     } else {
         dropdown.classList.add('closed');
-        svg.classList.remove('open');
+        svg?.classList.remove('open');
     }
 });
 
-async function getElectionsData(url = 'https://showcase.24sata.hr/izbori2024/parliamentary-elections-2024.json') {
+function autoRefresh() {
+    if (loaderWrapper) loaderWrapper.style.display = 'block';
+
+    getElectionsData(true);
+}
+
+async function getElectionsData(refresh = false) {
+    url = 'https://showcase.24sata.hr/izbori2024/parliamentary-elections-2024-latest.json';
+
     fetch(url).then((response) => {
         if (response.ok) {
             return response.json();
@@ -188,8 +216,26 @@ async function getElectionsData(url = 'https://showcase.24sata.hr/izbori2024/par
     .then((responseJson) => {
         electionResults = responseJson;
 
+        if (refresh) {
+            const menuItems = document.querySelectorAll('.menu_items li');
+
+            menuItems.forEach(item => {
+                if (item.textContent === selectedUnit.textContent) decideHowToProcess(item.dataset.unit);
+            });
+
+            setProcessedVotesAndLastChange(electionResults.status)
+
+            return;
+        }
+
+        dropdown = document.querySelector('.dropdown');
+        loaderWrapper = document.querySelector('.loader_wrapper');
+        targetUnit = document.querySelector('#target_unit');
+        selectedUnit = targetUnit;
+
         setLiveOrNot(responseJson.live);
         processData(responseJson);
+        setInterval(autoRefresh, 60000);
     })
     .catch((error) => {
         console.log(`Election results data were not retrieved due to: \n${error}`);
